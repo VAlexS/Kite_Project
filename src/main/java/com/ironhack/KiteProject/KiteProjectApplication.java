68ff4ce9ddba1ddbf9f1b10a9963891856eb97db
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-06 15:41:53 +0200

    commit inicial

---
+++ src/main/java/com/ironhack/KiteProject/KiteProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.KiteProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiteProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiteProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/KiteProject/KiteProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.KiteProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiteProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
