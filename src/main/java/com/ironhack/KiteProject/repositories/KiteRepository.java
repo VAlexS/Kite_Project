4174ec5609b11858c769218a9adfd0484a87f1ec
Víctor Sanz <vsanzc@alumnos.nebrija.es>
2025-05-09 13:51:34 +0200

    Busqueda cometas por dueño en el repositorio

--- src/main/java/com/ironhack/KiteProject/repositories/KiteRepository.java
+++ src/main/java/com/ironhack/KiteProject/repositories/KiteRepository.java
@@ -1,6 +1,7 @@
 package com.ironhack.KiteProject.repositories;
 
 import com.ironhack.KiteProject.models.kite.Kite;
+import com.ironhack.KiteProject.models.person.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,6 +9,9 @@ import java.util.List;
 public interface KiteRepository extends JpaRepository<Kite, Integer> {
 
     List<Kite> findKitesByLocation(String location);
+
+
+    List<Kite> findKitesByOwner(Person owner);
 
 
 
